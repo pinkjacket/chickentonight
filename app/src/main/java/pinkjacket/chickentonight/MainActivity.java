d142f9f0010f8519d67019edbbcab62ab7b6fa3c
Student <example@example.com>
2018-03-22 13:01:49 -0700

    navigation to empty list activity enabled

--- app/src/main/java/pinkjacket/chickentonight/MainActivity.java
+++ app/src/main/java/pinkjacket/chickentonight/MainActivity.java
@@ -1,13 +1,35 @@
 package pinkjacket.chickentonight;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.findRecipesButton) Button mFindRecipesButton;
+    @BindView(R.id.searchEditText) EditText mSearchEditText;
+    @BindView(R.id.titleTextView) TextView mTitleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mFindRecipesButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String search = mSearchEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, RecipeListActivity.class);
+                intent.putExtra("search", search);
+                startActivity(intent);
+            }
+        });
     }
 }
