ed8713240790008763fedde20f7451444d3e4eae
Student <example@example.com>
2018-03-22 13:31:37 -0700

    set up api credentials

---
+++ app/src/main/java/pinkjacket/chickentonight/Constants.java
@@ -0,0 +1,6 @@
+package pinkjacket.chickentonight;
+
+public class Constants {
+    public static final String YUMMLY_TOKEN = BuildConfig.YUMMLY_TOKEN;
+    public static final String YUMMLY_ID = BuildConfig.YUMMLY_ID;
+}
