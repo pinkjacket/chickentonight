9262a0410c38e2d133d26c4e4057a35308892335
Student <example@example.com>
2018-03-23 11:28:40 -0700

    recipe search now displays list of returned names

--- app/src/main/java/pinkjacket/chickentonight/RecipeListActivity.java
+++ app/src/main/java/pinkjacket/chickentonight/RecipeListActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
@@ -16,11 +20,13 @@ public class RecipeListActivity extends AppCompatActivity {
     public static final String TAG = RecipeListActivity.class.getSimpleName();
 
     public ArrayList<Recipe> recipes = new ArrayList<>();
+    @BindView(R.id.recipeListView) ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         String search = intent.getStringExtra("search");
@@ -41,6 +47,20 @@ public class RecipeListActivity extends AppCompatActivity {
 //                    Log.v(TAG, jsonData);
                     recipes = yumService.processResults(response);
                     Log.v("RESPONSE",  recipes.get(0).getName());
+
+                    RecipeListActivity.this.runOnUiThread(new Runnable(){
+                        @Override
+                        public void run(){
+                            String[] recipeNames = new String[recipes.size()];
+                            for(int i = 0; i<recipeNames.length; i++){
+                                recipeNames[i] = recipes.get(i).getName();
+                            }
+
+                            ArrayAdapter adapter = new ArrayAdapter(RecipeListActivity.this,
+                                    android.R.layout.simple_list_item_1, recipeNames);
+                            mListView.setAdapter(adapter);
+                        }
+                    });
                 }
         });
     }
