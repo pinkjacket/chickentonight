8d15cab47be3db2d051e4eb38afc31d8de7121f1
Student <example@example.com>
2018-03-30 12:56:12 -0700

    implemented firebase event listener

--- app/src/main/java/pinkjacket/chickentonight/MainActivity.java
+++ app/src/main/java/pinkjacket/chickentonight/MainActivity.java
@@ -6,13 +6,17 @@ import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @BindView(R.id.titleTextView) TextView mTitleTextView;
     @BindView(R.id.gitButton) Button mGitButton;
     private DatabaseReference mSearchedTermReference;
+    private ValueEventListener mSearchedTermReferenceListener;
 
 //    private SharedPreferences mSharedPreferences;
 //    private SharedPreferences.Editor mEditor;
@@ -34,6 +39,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .getInstance()
                 .getReference()
                 .child(Constants.FIREBASE_CHILD_SEARCHED_TERM);
+
+        mSearchedTermReferenceListener = mSearchedTermReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot searchSnapshot : dataSnapshot.getChildren()){
+                String search = searchSnapshot.getValue().toString();
+                Log.d("Searches updated", "search: " + search);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -66,6 +86,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void saveSearchToFirebase(String search){
         mSearchedTermReference.push().setValue(search);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        mSearchedTermReference.removeEventListener(mSearchedTermReferenceListener);
     }
 
 //    private void addToSharedPreferences(String search){
